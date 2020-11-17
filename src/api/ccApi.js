3000872c96cd69db4a91f11aacaf4c3524d42e6b
Surya1231 <suryaprakashagarwal245@gmail.com>
2020-11-17 10:12:41 +0530

    codechef link fixed

--- src/api/ccApi.js
+++ src/api/ccApi.js
@@ -4,10 +4,11 @@ import { getHtmlResponse } from './utilsApi';
 function extractContestFromTable(table, type, list) {
   for (let i = 1; i < table.rows.length; i++) {
     const row = table.rows[i];
+    //if (i < 2) console.dir(row.cells[1]);
     const contest = {
       code: row.cells[0].innerText,
       name: row.cells[1].children[0].innerText,
-      link: 'https://www.codechef.com/' + row.cells[1].children[0].href,
+      link: 'https://www.codechef.com/' + row.cells[1].children[0].pathname,
       startTime: row.cells[2].innerText,
       endTime: row.cells[3].innerText,
       phase: type,
